63ca3e9d5b28aedc5c9f3c75f37cfabd71ee7c4d
Saurabh Chavan <31591900+ssc134@users.noreply.github.com>
2019-08-23 13:01:31 +0530

    Add files via upload

---
+++ DarkPad_0.1/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace DarkPad_0._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.fileLabel = new System.Windows.Forms.ToolStripSplitButton();
+            this.newCtrlNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newWindowOpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openCtrlOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editLabel = new System.Windows.Forms.ToolStripDropDownButton();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.cutCtrlXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ctrlCtrlCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteCtrlVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.findCtrlFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.selectAllCtrlAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.insertDateTimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatLabel = new System.Windows.Forms.ToolStripLabel();
+            this.viewLabel = new System.Windows.Forms.ToolStripLabel();
+            this.helpLabel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileLabel,
+            this.editLabel,
+            this.formatLabel,
+            this.viewLabel,
+            this.helpLabel});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 31);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newCtrlNToolStripMenuItem,
+            this.newWindowOpenToolStripMenuItem,
+            this.openCtrlOToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(51, 28);
+            this.fileLabel.Text = "File";
+            // 
+            // newCtrlNToolStripMenuItem
+            // 
+            this.newCtrlNToolStripMenuItem.Name = "newCtrlNToolStripMenuItem";
+            this.newCtrlNToolStripMenuItem.Size = new System.Drawing.Size(293, 26);
+            this.newCtrlNToolStripMenuItem.Text = "New                       Ctrl+N";
+            // 
+            // newWindowOpenToolStripMenuItem
+            // 
+            this.newWindowOpenToolStripMenuItem.Name = "newWindowOpenToolStripMenuItem";
+            this.newWindowOpenToolStripMenuItem.Size = new System.Drawing.Size(293, 26);
+            this.newWindowOpenToolStripMenuItem.Text = "New Window       Ctrl+Shift+N";
+            // 
+            // openCtrlOToolStripMenuItem
+            // 
+            this.openCtrlOToolStripMenuItem.Name = "openCtrlOToolStripMenuItem";
+            this.openCtrlOToolStripMenuItem.Size = new System.Drawing.Size(293, 26);
+            this.openCtrlOToolStripMenuItem.Text = "Open                     Ctrl+O";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(293, 26);
+            this.saveToolStripMenuItem.Text = "Save                       Ctrl+S";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(293, 26);
+            this.saveAsToolStripMenuItem.Text = "Save As...               Ctrl+Shift+S";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(290, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(293, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editLabel
+            // 
+            this.editLabel.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.cutCtrlXToolStripMenuItem,
+            this.ctrlCtrlCToolStripMenuItem,
+            this.pasteCtrlVToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.findCtrlFToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.selectAllCtrlAToolStripMenuItem,
+            this.insertDateTimeToolStripMenuItem});
+            this.editLabel.Name = "editLabel";
+            this.editLabel.Size = new System.Drawing.Size(49, 28);
+            this.editLabel.Text = "Edit";
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(277, 26);
+            this.undoToolStripMenuItem.Text = "Undo                          Ctrl+Z";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(274, 6);
+            // 
+            // cutCtrlXToolStripMenuItem
+            // 
+            this.cutCtrlXToolStripMenuItem.Name = "cutCtrlXToolStripMenuItem";
+            this.cutCtrlXToolStripMenuItem.Size = new System.Drawing.Size(277, 26);
+            this.cutCtrlXToolStripMenuItem.Text = "Cut                             Ctrl+X";
+            // 
+            // ctrlCtrlCToolStripMenuItem
+            // 
+            this.ctrlCtrlCToolStripMenuItem.Name = "ctrlCtrlCToolStripMenuItem";
+            this.ctrlCtrlCToolStripMenuItem.Size = new System.Drawing.Size(277, 26);
+            this.ctrlCtrlCToolStripMenuItem.Text = "Copy                          Ctrl+C";
+            // 
+            // pasteCtrlVToolStripMenuItem
+            // 
+            this.pasteCtrlVToolStripMenuItem.Name = "pasteCtrlVToolStripMenuItem";
+            this.pasteCtrlVToolStripMenuItem.Size = new System.Drawing.Size(277, 26);
+            this.pasteCtrlVToolStripMenuItem.Text = "Paste                          Ctrl+V";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(277, 26);
+            this.deleteToolStripMenuItem.Text = "Delete                         Del  ";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(274, 6);
+            // 
+            // findCtrlFToolStripMenuItem
+            // 
+            this.findCtrlFToolStripMenuItem.Name = "findCtrlFToolStripMenuItem";
+            this.findCtrlFToolStripMenuItem.Size = new System.Drawing.Size(277, 26);
+            this.findCtrlFToolStripMenuItem.Text = "Find                            Ctrl+F";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(274, 6);
+            // 
+            // selectAllCtrlAToolStripMenuItem
+            // 
+            this.selectAllCtrlAToolStripMenuItem.Name = "selectAllCtrlAToolStripMenuItem";
+            this.selectAllCtrlAToolStripMenuItem.Size = new System.Drawing.Size(277, 26);
+            this.selectAllCtrlAToolStripMenuItem.Text = "Select All                    Ctrl+A";
+            // 
+            // insertDateTimeToolStripMenuItem
+            // 
+            this.insertDateTimeToolStripMenuItem.Name = "insertDateTimeToolStripMenuItem";
+            this.insertDateTimeToolStripMenuItem.Size = new System.Drawing.Size(277, 26);
+            this.insertDateTimeToolStripMenuItem.Text = "Insert Date/Time        F5";
+            // 
+            // formatLabel
+            // 
+            this.formatLabel.Name = "formatLabel";
+            this.formatLabel.Size = new System.Drawing.Size(56, 28);
+            this.formatLabel.Text = "Format";
+            // 
+            // viewLabel
+            // 
+            this.viewLabel.Name = "viewLabel";
+            this.viewLabel.Size = new System.Drawing.Size(41, 28);
+            this.viewLabel.Text = "View";
+            // 
+            // helpLabel
+            // 
+            this.helpLabel.Name = "helpLabel";
+            this.helpLabel.Size = new System.Drawing.Size(41, 28);
+            this.helpLabel.Text = "Help";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSplitButton fileLabel;
+        private System.Windows.Forms.ToolStripMenuItem newCtrlNToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newWindowOpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openCtrlOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton editLabel;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem cutCtrlXToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ctrlCtrlCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteCtrlVToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem findCtrlFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem selectAllCtrlAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insertDateTimeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripLabel formatLabel;
+        private System.Windows.Forms.ToolStripLabel viewLabel;
+        private System.Windows.Forms.ToolStripLabel helpLabel;
+    }
+}
+
